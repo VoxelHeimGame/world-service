0e98ec728ab712d9eca8a3e2a6adf2e58174f82c
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-12-03 22:00:46 +0100

    chore: first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "World Service").listen(3001);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🗺️ World Service is running at ${app.server?.hostname}:${app.server?.port}`
 );
